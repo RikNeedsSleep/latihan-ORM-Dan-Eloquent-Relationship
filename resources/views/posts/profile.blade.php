270f9c2c24c2e8a281d016d40f9db34c41b43d8a
Rik <142599233+RikNeedsSleep@users.noreply.github.com>
2024-05-08 10:05:27 +0700

    Add files via upload

---
+++ resources/views/posts/profile.blade.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+    <div class="container mt-5">
+        <div class="row justify-content-center">
+            <div class="col-md-6">
+                <div class="card">
+                    <div class="card-header bg-primary text-white">
+                        User Profile
+                    </div>
+                    <div class="card-body">
+                        <h5 class="card-title">erik</h5>
+                        <p class="card-text">Email: erik@gmail.com</p>
+                        <p class="card-text">Gender: laki-laki</p>
+                        <p class="card-text">Age: 25</p>
+                        <p class="card-text">Date of Birth: January 1, 1990</p>
+                        <p class="card-text">Address: 123 Street, City</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="card">
+                    <div class="card-header bg-success text-white">
+                        Merchant Profile
+                    </div>
+                    <div class="card-body">
+                        <h5 class="card-title">stark company</h5>
+                        <p class="card-text">Rate: 4.5</p>
+                        <p class="card-text">Best Product: batrai</p>
+                        <p class="card-text">Description: Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+    
+</body>
+</html>
